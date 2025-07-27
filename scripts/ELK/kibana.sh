561e320f3f4186c55300ce5b6b32d64471f13af4
Minh Huy <huy.cse.9@gmail.com>
2025-07-27 21:57:48 +0700

    Add ingress for Kibana

--- scripts/ELK/kibana.sh
+++ scripts/ELK/kibana.sh
@@ -3,5 +3,6 @@
 NAMESPACE="logging"
 
 echo "Creating service..."
-helm upgrade --install kibana helm/ELK/kibana -f helm/ELK/kibana/my-values.yaml --namespace $NAMESPACE
+helm upgrade --install kibana helm/ELK/kibana -f helm/ELK/kibana/my-values.yaml --namespace $NAMESPACE\
+             --set ingress.hosts[0].host="kibana.$EXTERNAL_IP.nip.io"
 
--- scripts/prometheus-stack.sh
+++ scripts/prometheus-stack.sh
@@ -3,4 +3,5 @@
 NAMESPACE="monitoring"
 
 echo "Creating service..."
-helm upgrade --install kube-prom-stack helm/kube-prometheus-stack --namespace $NAMESPACE
+helm upgrade --install kube-prom-stack helm/kube-prometheus-stack --namespace $NAMESPACE\
+             --set grafana.ingress.hosts[0]="grafana.$EXTERNAL_IP.nip.io"
