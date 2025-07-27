d9c455de7c8908b4e3e2b6063ec1500fa0af5b30
Minh Huy <huy.cse.9@gmail.com>
2025-07-27 17:09:18 +0700

    Feat: Add nginx ingress for Jaeger

--- scripts/jaeger.sh
+++ scripts/jaeger.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-NAMESPACE="monitoring"
-
 echo "Creating service..."
-helm upgrade --install jaeger helm/jaeger --namespace $NAMESPACE
+helm upgrade --install jaeger helm/jaeger --namespace monitoring\
+             --set allInOne.ingress.hosts[0]="jaeger.${EXTERNAL_IP}.nip.io"
