f971322f4477f536bf1ae498141c19470b4fed7c
Minh Huy <huy.cse.9@gmail.com>
2025-08-10 15:04:37 +0700

    feat: Update name for model-serving release, add new architecture

--- scripts/model-serving.sh
+++ scripts/model-serving.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-helm upgrade --install promt-guardrail ./helm/prompt-guardrail \
+helm upgrade --install prompt-guardrail ./helm/prompt-guardrail \
     --namespace model-serving
     # --set ingress.host="app.${EXTERNAL_IP}.nip.io"
